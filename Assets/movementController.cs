38185f4113e14924e3c221960ac7b4de0bba907e
anmont <anmont@gmail.com>
2021-11-02 21:27:38 -0600

    udpate for the night

--- Assets/movementController.cs
+++ Assets/movementController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class movementController : MonoBehaviour
 {
+    GameObject Player;
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = .66f;
+    private float rotationSpeed = 100f;
     private float playerRunSpeed = 1.33f;
     private float jumpHeight = .33f;
     private float gravityValue = -6.81f;
@@ -15,41 +17,77 @@ public class movementController : MonoBehaviour
     private void Start()
     {
         //controller = this.parent.gameobject.getComponent<CharacterController>();
+        Player = GameObject.Find("Player");
         controller = gameObject.GetComponent<CharacterController>();
         //gameObject.AddComponent<CharacterController>();
     }
 
     void Update()
     {
+        //Jump
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            controller.Move(move * Time.deltaTime * playerRunSpeed);
-        }
-        else
-        {
-            controller.Move(move * Time.deltaTime * playerSpeed);
-        }
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // Changes the height position of the player..
-        //if (Input.GetButtonDown("Jump") && groundedPlayer)
         if (Input.GetKey(KeyCode.Space) && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
+        //Gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+
+        //Left and Right
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Vector3 move = Vector3.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                move = Player.transform.rotation * Vector3.left;
+            }
+            else
+            {
+                move = Player.transform.rotation * Vector3.right;
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(move * Time.deltaTime * playerRunSpeed);
+            }
+            else
+            {
+                controller.Move(move * Time.deltaTime * playerSpeed);
+            }
+        }
+        // Forward and back
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            Vector3 move = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                move = Player.transform.rotation * Vector3.forward;
+            }
+            else
+            {
+                move = Player.transform.rotation * Vector3.back;
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(move * Time.deltaTime * playerRunSpeed);
+            }
+            else
+            {
+                controller.Move(move * Time.deltaTime * playerSpeed);
+            }
+        }
+
+        //Mouse look left and right
+        if(Input.GetMouseButton(1))
+        {
+            Vector3 rotation = new Vector3(0f, Input.GetAxis("Mouse X") * rotationSpeed * 10 * Time.deltaTime, 0f);
+            Player.transform.Rotate(rotation);
+            //Debug.Log(rotation.ToString());
+        }
     }
 }
--- Assets/roomGeneration.cs
+++ Assets/roomGeneration.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class roomGeneration : MonoBehaviour
 {
+    public GameObject towerParent;
+    public GameObject roomPrefab;
+    public int roomCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
+        //setup first room
+        GameObject initialRoom = generateRoom(5,5,5);
+        initialRoom.transform.position = new Vector3(3.5f,2.5f,3.5f);
+
+
         
     }
 
@@ -16,8 +24,14 @@ public class roomGeneration : MonoBehaviour
         
     }
 
-    public void generateRoom(int x, int y)
+    public GameObject generateRoom(float x, float y, float z)
     {
-
+        //GameObject myRoom = (GameObject)Resources.Load("Assets/_Prefabs/Room.prefab");
+        GameObject newRoom = null;
+        newRoom = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity,towerParent.transform);
+        newRoom.gameObject.name = "Room" + roomCounter.ToString();
+        newRoom.transform.localScale = new Vector3(x,y,z);
+        roomCounter ++;
+        return newRoom;
     }
 }
